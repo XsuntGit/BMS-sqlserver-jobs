966ec51b920af932d702e32c3928308d89a232fd
svc-xsunt-jenkins <svc-jenkins@xsunt.com>
2023-09-25 22:25:35 -0400

    Push latest 2023-09-25 10:25:35

--- 172.25.99.101/1APS PrvWkUserDepthTblbkp.sql
+++ 172.25.99.101/1APS PrvWkUserDepthTblbkp.sql
@@ -103,7 +103,7 @@ IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Backup Heme version', 
 		@step_id=2, 
 		@cmdexec_success_code=0, 
-		@on_success_action=1, 
+		@on_success_action=3, 
 		@on_success_step_id=0, 
 		@on_fail_action=2, 
 		@on_fail_step_id=0, 
@@ -172,6 +172,86 @@ End catch
     @profile_name = ''XSUNT ONELOOK-DB-1 SQL NOTIFICATION'',
 	----@recipients = ''prateek.singh@xsunt.com'',
     @recipients = ''prateek.singh@xsunt.com;nguillot@xsunt.com'',
+ 
+    @subject = @sub,
+    @body = @body1 ,
+    @body_format=''HTML''', 
+		@database_name=N'master', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'backup triaalist reb1l', 
+		@step_id=3, 
+		@cmdexec_success_code=0, 
+		@on_success_action=1, 
+		@on_success_step_id=0, 
+		@on_fail_action=2, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'TSQL', 
+		@command=N'
+Use BMSHEME3
+GO
+
+
+declare @CurrDate date
+	declare @Sql varchar(8000)
+	declare @FileLocation varchar(255)
+	,@txt1 as varchar(4000) ,@txt2 as varchar(max) , @rowcount as bigint, @maxdate as varchar(20)
+	,@sub nvarchar(100)
+ DECLARE @html nvarchar(MAX), @body1 Nvarchar(max) , @int int, @dt1 nvarchar(25)
+   select @int = case when @html is not null then 1 else 0 end 
+ select @dt1 =  Convert(nvarchar(25),getdate())
+
+Begin try
+if object_id(N''tblMDSClaimHCP_TrialistPrv'',N''U'') is not null
+	drop table tblMDSClaimHCP_TrialistPrv
+Select * into tblMDSClaimHCP_TrialistPrv
+from
+BMSHEME3.dbo.tblMDSClaimHCP
+
+--where [Target] = ''Y'' and SalesTgt = ''Y''
+
+      
+	  set @sub= ''Success -  Prv Week REB1L Trialist Report Table Backed up as of : ''
+	  Select @rowcount=count(*) from tblMDSClaimHCP_TrialistPrv
+ 	  set @txt2 = ''No. of Records in  <Strong> tblMDSClaimHCP_TrialistPrv  </Strong> --->   <Strong>'' +  cast(format(@rowcount  , ''N0'') as varchar) +''</Strong>''
+	  
+    set @body1 =   ''Hi Team,<br> <br> Prv Week REB1L Trialist Report Table Backed  as of : <Strong>''+@dt1+''</Strong> <br><br>''
+    +@txt2 +'' <br><br><br><br>  Thanks , <br> HEME Team <br>'' 
+
+End try
+
+Begin catch
+
+
+  set @body1 =  ''Hi Team,  <br><br>''
+  +''There is an error in  Prv Week REB1L Trialist Report Table  backup processing as below.Please look into the same. <br><br>''
+ +''<b> ERROR_NUMBER: ''+cast( ERROR_NUMBER() as varchar)+''| ''+
+ ''ERROR_SEVERITY: ''+cast( ERROR_SEVERITY() as varchar)+''| ''+
+ ''ERROR_STATE: ''+cast( ERROR_STATE() as varchar)+''| ''+
+ ''ERROR_LINE: ''+cast( ERROR_LINE() as varchar)+''| ''+
+ ''ERROR_MESSAGE: ''+convert( varchar(8000), ERROR_MESSAGE() )
+ +''</b>  <br><br>
+ Thanks , <br>
+OneLookOncology Team <br>'' 
+ 
+   set @sub = ''Error -  Prv Week REB1L Trialist Report Table Backed up as of : '' +@dt1
+  set @body1 =  @body1
+
+
+
+End catch
+
+
+ 
+ 
+   set @sub = @sub +@dt1
+  set @body1 =  @body1
+  EXEC msdb.dbo.sp_send_dbmail
+    @profile_name = ''XSUNT ONELOOK-DB-1 SQL NOTIFICATION'',
+	----@recipients = ''prateek.singh@xsunt.com'',
+    @recipients = ''prateek.singh@xsunt.com'',
  
     @subject = @sub,
     @body = @body1 ,
