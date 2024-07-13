c1e1f03fcaf301dd581b6cc26aba9eee996e8466
svc-xsunt-jenkins <svc-jenkins@xsunt.com>
2024-07-12 22:51:47 -0400

    Push latest 2024-07-12 10:51:47

---
+++ 172.28.99.39/1APS NEURO Weekly Run.sql
@@ -0,0 +1,69 @@
+﻿BEGIN TRANSACTION
+DECLARE @ReturnCode INT
+SELECT @ReturnCode = 0
+IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Uncategorized (Local)]' AND category_class=1)
+BEGIN
+EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Uncategorized (Local)]'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+END
+
+DECLARE @jobId BINARY(16)
+EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'1APS_NEURO_Weekly_Run', 
+		@enabled=1, 
+		@notify_level_eventlog=0, 
+		@notify_level_email=2, 
+		@notify_level_netsend=0, 
+		@notify_level_page=0, 
+		@delete_level=0, 
+		@description=N'No description available.', 
+		@category_name=N'[Uncategorized (Local)]', 
+		@owner_login_name=N'XSUNT\prateek.singh', 
+		@notify_email_operator_name=N'Job Failure Notification', @job_id = @jobId OUTPUT
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'RUN NEURO WEEKLY', 
+		@step_id=1, 
+		@cmdexec_success_code=0, 
+		@on_success_action=1, 
+		@on_success_step_id=0, 
+		@on_fail_action=2, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'TSQL', 
+		@command=N'Declare @sql varchar(8000)
+Declare @str1 varchar(1000)
+set @str1 = cast(FORMAT(GETDATE() , ''yyyyMMdd_HHmmss'') as varchar)
+
+	set @Sql = ''SQLCMD -S OneLook-DB-DR-1 -i "W:\work\scripts\BMS\OneLookDataProcess\NEURO\WeeklyRun\WeeklyRun.sql" -o "W:\work\scripts\BMS\OneLookDataProcess\NEURO\WeeklyRun\Logs\NEURO_WeeklyRun_''+@str1+''.txt"''
+	EXEC master.sys.xp_cmdshell @Sql
+', 
+		@database_name=N'master', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'RUN WEEKLY SCHEDULE NEURO', 
+		@enabled=1, 
+		@freq_type=8, 
+		@freq_interval=33, 
+		@freq_subday_type=1, 
+		@freq_subday_interval=0, 
+		@freq_relative_interval=0, 
+		@freq_recurrence_factor=1, 
+		@active_start_date=20240712, 
+		@active_end_date=99991231, 
+		@active_start_time=230000, 
+		@active_end_time=235959, 
+		@schedule_uid=N'66a4c69c-f919-4845-b84e-8480947f0a69'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+COMMIT TRANSACTION
+GOTO EndSave
+QuitWithRollback:
+    IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
+EndSave:
+
+GO
+
