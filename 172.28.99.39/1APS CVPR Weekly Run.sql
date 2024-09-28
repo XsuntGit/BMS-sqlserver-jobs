fb578877a9b53102712258df8e17bdf075f1929a
svc-xsunt-jenkins <svc-jenkins@xsunt.com>
2024-09-27 22:52:26 -0400

    Push latest 2024-09-27 10:52:26

--- 172.28.99.39/1APS CVPR Weekly Run.sql
+++ 172.28.99.39/1APS CVPR Weekly Run.sql
@@ -46,14 +46,14 @@ IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'CV PR WEEKLY RUN SCHEULE', 
 		@enabled=1, 
 		@freq_type=8, 
-		@freq_interval=32, 
+		@freq_interval=64, 
 		@freq_subday_type=1, 
 		@freq_subday_interval=0, 
 		@freq_relative_interval=0, 
 		@freq_recurrence_factor=1, 
 		@active_start_date=20240125, 
 		@active_end_date=99991231, 
-		@active_start_time=210100, 
+		@active_start_time=90100, 
 		@active_end_time=235959, 
 		@schedule_uid=N'ba1ad2be-467f-44e3-9cd7-0bb5c71c187e'
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
